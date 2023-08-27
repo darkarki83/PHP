ae62458e1d038af90e58ce8e979836fb51295dea
Artem Krol <artemkrol47@gmail.com>
2023-08-27 09:29:24 +0200

    Composer finished

--- PhpDocker/src/public/index.php
+++ PhpDocker/src/public/index.php
@@ -2,17 +2,17 @@
 
 declare(strict_types=1);
 
-spl_autoload_register(function ($class) {
-    $path = __DIR__ . '/../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
-
-    if (file_exists($path)) {
-        require $path;
-    }   
-});
+require __DIR__ . '/../vendor/autoload.php';
 
 use App\PaymentGateway\Paddle\{Transaction, CustomerProfile};
 
 $transaction = new Transaction(100);
+
+//var_dump($transaction);
+
+$id = new \Ramsey\Uuid\UuidFactory();
+
+echo $id->uuid4();
 
 var_dump($transaction);
 
